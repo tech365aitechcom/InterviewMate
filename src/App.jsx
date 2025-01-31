94a10045e30cf666e7a0997b6b29d0863057a679
SougataMondal2000 <sougataofficial362000@gmail.com>
2025-01-31 11:46:54 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import AgentInterview from "./pages/AgentInterview.jsx";
 import MockInterviewQuiz from "./pages/MockInterviewQuiz.jsx";
+import NewAgentInterview from "./pages/NewAgentInterview.jsx";
 
 function App() {
   return (
-    <div className="w-full container">
+    <div className="w-full">
       <Router>
         <Routes>
-          <Route path="/mock-interview/agent" element={<AgentInterview />} />
+          <Route path="/mock-interview/agent" element={<NewAgentInterview />} />
           <Route path="/mock-interview/quiz" element={<MockInterviewQuiz />} />
         </Routes>
       </Router>
